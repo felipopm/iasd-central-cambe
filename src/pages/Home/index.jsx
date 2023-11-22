3450b0b40bab098ea8ff6d9f44b83c111f1a0998
Felipe <139351148+felipopm@users.noreply.github.com>
2023-11-22 12:47:02 -0300

    estilizando pagina Home/ IASD

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,34 +3,29 @@ import Footer from '../../components/Footer'
 import Container from '../../components/Container';
 import styles from './Home.module.css'
 
-function Home(){
-    return(
-        <>
-        <Header />
-        <Container>
-          <section className={styles.home}>
-            {/* <div className='perguntas'>
-              <h1>Qual é o propósito da vida segundo Deus?</h1>
-              <h1>Existe inferno?</h1>
-              <h1>Deus existe?</h1>
-              <h1>Se Deus existe porque existe dor?</h1>
-              <h1>Se Deus existe porque existe dor?</h1>
-              <h1>Quem são os adventista?</h1>
-              <h1>Como ter um relacionamento com Deus?</h1>
-              <h1>Quem é Deus?</h1>
-            </div> */}
+function Home() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <section className={styles.home}>
 
-            {/* <div className={styles.pesquisa}>
-              <input 
-              type="search" 
-              placeholder='Pergunte algo?' />
-            </div> */}
+          <div className={styles.noticias}>
+            <h3>Notícias</h3>
+          </div>
 
-          </section>
-        </Container>
-        <Footer />
-      </>
-    );
+          <div className={styles.novo}>
+            <h3>Novo</h3>
+            <div>
+              Conteúdo
+            </div>
+          </div>
+
+        </section>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default Home
